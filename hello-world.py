735d337d8e13453a85f5635ce731f2ab7ef80783
Dilip Vishwanath <dilipkv@hotmail.com>
2025-05-26 11:01:02 -0500

    this is a test of python on my local workspace

---
+++ hello-world.py
@@ -0,0 +1,6 @@
+
+# This script prints "Hello, World!" to the console.
+# It serves as a simple demonstration of Python syntax and output.
+# You can run this script in any Python environment.
+# To execute this script, save it as hello.py and run it using the command:
+print("Hello, World!")
