8db223c8bfdbe95f8300e67614c74cd487737639
Dilip Vishwanath <dilipkv@hotmail.com>
2025-05-26 11:22:24 -0500

    Changed to Dilip

--- hello-world.py
+++ hello-world.py
@@ -3,4 +3,4 @@
 # It serves as a simple demonstration of Python syntax and output.
 # You can run this script in any Python environment.
 # To execute this script, save it as hello.py and run it using the command:
-print("Hello, World!")
+print("Hello, Dilip!")
